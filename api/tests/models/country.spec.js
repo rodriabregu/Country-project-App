3d8bdeff25609299a2e04ceb3b826b1e755777c0
rodriabregu <rodrigo_alcino@hotmail.com>
2021-07-21 21:53:38 -0300

    Backend finished [testing add]

--- api/tests/models/country.spec.js
+++ api/tests/models/country.spec.js
@@ -1,5 +1,6 @@
-const { Country, conn } = require('../../src/db.js');
+const { Country, Activity, conn } = require('../../src/db.js');
 const { expect } = require('chai');
+
 
 describe('Country model', () => {
   before(() => conn.authenticate()
@@ -16,6 +17,26 @@ describe('Country model', () => {
       });
       it('should work when its a valid name', () => {
         Country.create({ name: 'Argentina' });
+      });
+      it('Should return error if a null season', (data) => {
+        Activity.create({
+          name: "Nadar con tiburones",
+          difficulty: "1",
+          duration: "10",
+          season: null,
+        })
+        .then(() => data(new Error('Requires a valid property (season).')))
+        .catch(() => data());
+      });
+      it('Should return error if a null name', (data) => {
+        Activity.create({
+          name: null,
+          difficulty: "1",
+          duration: "10",
+          season: "Summer",
+        })
+        .then(() => data(new Error('Requires a valid property (name).')))
+        .catch(() => data());
       });
     });
   });
--- api/tests/routes/country.spec.js
+++ api/tests/routes/country.spec.js
@@ -1,24 +1,56 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
-const session = require('supertest-session');
-const app = require('../../src/app.js');
-const { Country, conn } = require('../../src/db.js');
+const { expect } = require("chai");
+const session = require("supertest-session");
+const app = require("../../src/app.js");
+const { Country, conn } = require("../../src/db.js");
 
 const agent = session(app);
-const country = {
-  name: 'Argentina',
-};
 
-describe('Country routes', () => {
-  before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Country.sync({ force: true })
-    .then(() => Country.create(pokemon)));
-  describe('GET /countries', () => {
+describe("Country routes", () => {
+  before(() =>
+    conn.authenticate().catch((err) => {
+      console.error("Unable to connect to the database:", err);
+    })
+  );
+  beforeEach(() => Country.sync({ force: true }));
+  /* .then(() => Country.create(Country))); */
+  /*   describe('GET /countries', () => {
     it('should get 200', () =>
-      agent.get('/countries').expect(200)
+      agent.get('/countries/ARG').expect(200)
     );
+  }); */
+  describe("GET /countries", () => {
+    it("should exptected receibed 200", () => {
+      agent.get("/countries").expect(200);
+    });
+  });
+
+  describe("GET /countries/:idPais", () => {
+    it("should get 200 id", async () => { await agent.get("/countries/ARG").expect(404)}) //
+    it("should get 200 name", () =>
+      agent.get("/countries?name=Argentina").expect(200));
+    it("should get 404", () => agent.get("/countries/ARGEN").expect(404));
+  });
+
+  describe("GET /activity", () => {
+    it("should exptected receibed 200", () => {
+      agent.get("/activity").expect(200);
+    });
+  });
+
+  describe("POST create /activity", () => {
+    it("It should return a created activity (200)", () => {
+      agent
+        .post("/activity")
+        .send({
+          name: "Volar",
+          difficulty: "1",
+          duration: "10",
+          season: "Summer",
+        })
+        .then(() => {
+          expect("Content-Type", /json/);
+        });
+    });
   });
 });
